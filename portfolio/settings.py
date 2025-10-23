64f5f3d65d413bed849c302907cba5076cec52b1
Nishant <ghimirenishant2060@gmail.com>
2025-10-24 01:05:34 +0545

    Fix ALLOWED_HOSTS for Render-6

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'portfolio.urls'
 
