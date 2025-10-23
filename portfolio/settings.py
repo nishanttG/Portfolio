e2cc39ccf06b3dfea03918dd4f47cf26af6d3305
Nishant <ghimirenishant2060@gmail.com>
2025-10-24 01:45:39 +0545

    final

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -113,9 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL='/images/'
-
-STATICFILES_DIRS=[
-  os.path.join(BASE_DIR, 'static')  
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # collectstatic will copy here
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
