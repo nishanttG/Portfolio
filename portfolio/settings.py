48bfef85dfe71fb3bcaaf4199637d4b1fad0ac3e
Nishant <ghimirenishant2060@gmail.com>
2025-10-24 00:44:12 +0545

    Fix ALLOWED_HOSTS for Render-2

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -129,9 +129,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')  # where your static files live during dev
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # where collectstatic will copy everything
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
